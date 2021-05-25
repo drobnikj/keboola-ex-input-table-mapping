9f3968c68b751cf9706b53f69fd8b9344db2ca2e
drobnikj <drobnik.j@gmail.com>
2021-05-25 17:02:20 +0200

    Init actor commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# keboola-ex-input-table-mapping
-Toolkit actor that parses input table from Apify keboola extractor and maps the data into another task or actor input.
+# Keboola Input mapping
+
+Actor parses input table from Apify <> Keboola extractor and maps the data into another task or actor input.
+
+<!-- toc start -->
+- Introduction
+- Use Case - Web Scraper
+- Input
+ <!-- toc end -->
+
+## Introduction
+
+It is not possible pass input table from Keboola extractor into any public actor. Because Keboola extractor passed
+input table using key-value store and the storeId and key of the input table puts into actor/task input. And because
+storeId and key is not part of any public actor and will be omitted. This tool helps you to run actor/task from Keboola
+extractor and map the input table into actor/task input.
+
+## Use Case - Web Scraper
+
+Imagine we have in Keboola tabled data in CSV in this format:
+```csv
+Title,URL,ID
+Test1,http://example.com#1,product_1
+Test2,http://example.com#2,product_2
+Test3,http://example.com#3,product_3
+Test4,http://example.com#4,product_4
+Test5,http://example.com#5,product_5
+```
+We need to run the task of apify/web-scraper, where URL column will be use on input as "Start URLs".
+In this case we will create new task of this actor, we can use this link. We will fill
+"Target Task ID" with name of created web-scraper task and
+"Input mapping function" with simple Javascript function which maps CSV into input.
+```javascript
+function inputMappingFunction({ currentInput, parsedInputTableCsv }) {
+    const startUrls = parsedInputTableCsv.map((line) => {
+        return { url: line['URL'] };
+    });
+    return {
+        startUrls,
+    };
+}
+```
+We will save the task and go to Keboola and use created task in Apify extractor and select table which we
+want to pass.
+![Keboola-ex](./keboola-ex.png)
+After save and run this configuration the task should be run our target task and use input created in mapping function.
+
+## Miscellaneous
+
+If you are struggling with the Apify <> Keboola setup, please contact me or [Apify support](mailto:support@apify.com).
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,82 @@
+const Apify = require('apify');
+const parse = require('csv-parse');
+const _ = require('lodash');
+const vm = require('vm');
+
+const apifyClient = Apify.newClient();
+
+Apify.main(async () => {
+    const input = await Apify.getInput();
+    const { inputTableRecord: { storeId, key }, fields, inputMapping, targetActorId, targetTaskId } = input;
+
+    /**
+     * NOTE: It is not possible to metamorf into task. But we get task input and actor ID for task
+     * and use to metamerf into.
+     */
+    let metamorfActorId;
+    let metamorfInput;
+    let metamorfOptions;
+    if (targetActorId) {
+        try {
+            const actor = await apifyClient.actor(targetActorId).get();
+            metamorfActorId = actor.id;
+            metamorfInput = {};
+        } catch (err) {
+            throw new Error(`Cannot find actor with ${targetActorId}!`);
+        }
+    } else if (targetTaskId) {
+        try {
+            const task = await apifyClient.task(targetTaskId).get();
+            metamorfActorId = task.actId;
+            metamorfInput = task.input;
+            metamorfOptions = task.options;
+            console.log(task);
+        } catch (err) {
+            throw new Error(`Cannot find task with ${targetTaskId}!`);
+        }
+    } else {
+        throw new Error('Target ActorId or TaskId has to be set!');
+    }
+
+    // Use apify-client, SDK cannot do streams.
+    const storeClient = await apifyClient.keyValueStore(storeId);
+    const { value: recordStream } = await storeClient.getRecord(key, { stream: true });
+
+    // Parse csv using with async generators
+    const parser = recordStream.pipe(parse({ columns: true }));
+
+    const parsedCsvData = [];
+    for await (const line of parser) {
+        const updatedLine = Array.isArray(fields) && fields.length
+            ? _.pick(line, fields)
+            : line;
+        parsedCsvData.push(updatedLine);
+        // TODO: ?? Function that can be eval on each line
+    }
+
+    let inputMappingFunction;
+    try {
+        inputMappingFunction = vm.runInThisContext(`(${inputMapping})`);
+    } catch (err) {
+        throw new Error(`Cannot load inputMapping function: ${err.message}\n${err.stack.substr(err.stack.indexOf('\n'))}`);
+    }
+    if (!_.isFunction(inputMappingFunction)) throw new Error('Parameter "inputMapping" is not a function!');
+
+    const context = {
+        originalInput: input,
+        parsedCsvData,
+    };
+    const inputMapped = await inputMappingFunction(context);
+
+    if (!_.isObject(inputMapped)) throw new Error('The inputMapping does not return object. It needs to return object as input for target actor/task run.');
+
+    const finalInput = { ...metamorfInput, ...inputMapped };
+    // TODO: !!! This is workaround as Apify.metamorph did not use input as the second parameter, check why and report !!!
+    await Apify.setValue('INPUT', finalInput);
+    const finalOptions = metamorfOptions && metamorfOptions.build ? { build: metamorfOptions.build } : {};
+    await Apify.metamorph(metamorfActorId, finalInput, finalOptions);
+});
+
+parsedInputTableCsv.map((line) => {
+    return { url: line['URL'] };
+});
